834c15b52b73582a03b8c3d4d19f7673b9fb150c
philiprurka <hey@philiprurka.com>
2024-04-29 15:58:11 -0400

    package & vercel.json and next.config adjust

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  poweredByHeader: false,
+  onDemandEntries: {
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
+  },
   images: {
     loader: "custom",
     formats: ["image/avif", "image/webp"],
