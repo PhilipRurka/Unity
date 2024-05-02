33ab7e2cd1dfca94e865fc82c99fe1429282bf04
Philip Rurka <hey@philiprurka.com>
2024-05-02 16:44:26 -0400

    add init contentful api

---
+++ app/api/contentful/getByContentModel/methods/GET.ts
@@ -0,0 +1,28 @@
+// import client from '@/Lib/createClient';
+import client from '@/Lib/createClient';
+
+type SuccessGet = [{ data: any }, { status: number }];
+type ErrorGet = [{ data: { message: string } }, { status: number }];
+
+type GetByContentModel = (reqData: { contentModel: string }) => Promise<SuccessGet | ErrorGet>;
+
+type CatchError = {
+  message: string;
+};
+
+const getByContentModel: GetByContentModel = async (reqData) => {
+  try {
+    const { contentModel } = reqData;
+    console.log(contentModel);
+
+    client().getContentType(contentModel);
+
+    return [{ data: 'any' }, { status: 200 }];
+  } catch (err) {
+    const error = err as CatchError;
+
+    return [{ data: { message: error.message } }, { status: 503 }];
+  }
+};
+
+export default getByContentModel;
