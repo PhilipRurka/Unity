e9d9816c242536b6aa1cabb3874ec228313b2ad9
Philip Rurka <hey@philiprurka.com>
2024-05-06 13:40:27 -0400

    adjust markdown and its styles

--- lib/markdown.tsx
+++ lib/markdown.tsx
@@ -1,13 +1,22 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-import { BLOCKS } from '@contentful/rich-text-types';
+import { BLOCKS, INLINES } from '@contentful/rich-text-types';
+import Link from 'next/link';
 
 type MarkdownType = { content: any };
 
-const Markdown = ({ content }: MarkdownType) =>
-  documentToReactComponents(content, {
-    renderNode: {
-      [BLOCKS.HEADING_2]: (_node, children) => <h2 className="text-4xl">{children}</h2>,
-    },
-  });
+const Markdown = ({ content }: MarkdownType) => (
+  <div data-component="cMarkdown">
+    {documentToReactComponents(content, {
+      renderNode: {
+        [INLINES.HYPERLINK]: (node, children) => (
+          <Link className="inline" href={node.data.uri}>
+            {children}
+          </Link>
+        ),
+        [BLOCKS.HEADING_2]: (_node, children) => <h2 className="mb-4 text-3xl">{children}</h2>,
+      },
+    })}
+  </div>
+);
 
 export default Markdown;
