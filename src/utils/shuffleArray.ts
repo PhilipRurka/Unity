6545dbbd233685775e37fb22cbaea0a1f0014269
Philip Rurka <hey@philiprurka.com>
2024-05-07 16:09:08 -0400

    add shuffleArray utils function

---
+++ src/utils/shuffleArray.ts
@@ -0,0 +1,13 @@
+const shuffleArray = <T>(arr: T[]): T[] => {
+  const array = [...arr];
+
+  for (let i = array.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+
+  return array;
+};
+
+export default shuffleArray;
