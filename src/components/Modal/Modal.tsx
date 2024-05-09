be4e159ca3c8296b7ca22bafc49678f621e4fd73
Philip Rurka <hey@philiprurka.com>
2024-05-09 10:58:31 -0400

    create Modal

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,44 @@
+import clsx from 'clsx';
+import { ReactNode } from 'react';
+
+import { CloseIcon } from '@/Components/Icons';
+
+const sizes = {
+  sm: 150,
+  md: 200,
+  lg: 250,
+};
+
+type ModalProps = {
+  children: ReactNode;
+  width: 'sm' | 'md' | 'lg';
+  handleCloseModal: () => void;
+};
+
+const Modal = ({ children, width, handleCloseModal }: ModalProps) => {
+  const handleTriggerClose = () => {
+    handleCloseModal();
+  };
+
+  const size = `w-${sizes[width]}`;
+
+  return (
+    <div className="fixed inset-0 z-10">
+      <div className="absolute inset-0 bg-black opacity-80" onClick={handleTriggerClose} />
+      <div
+        className={clsx(
+          'absolute left-1/2 top-1/3 -translate-x-1/2 -translate-y-1/2 transform',
+          'max-w-2xl rounded-xl bg-white p-8',
+          size
+        )}
+      >
+        <button onClick={handleTriggerClose}>
+          <CloseIcon size="8" />
+        </button>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
