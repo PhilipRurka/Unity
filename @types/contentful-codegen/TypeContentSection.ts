abbc3bfe510687e47b67f26e49fc2ae6a8e71572
Philip Rurka <hey@philiprurka.com>
2024-05-10 12:38:21 -0400

    update Contentful Types

--- @types/contentful-codegen/TypeContentSection.ts
+++ @types/contentful-codegen/TypeContentSection.ts
@@ -2,6 +2,7 @@ import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleC
 
 export interface TypeContentSectionFields {
     entryTitle: EntryFieldTypes.Symbol;
+    title?: EntryFieldTypes.Symbol;
     content: EntryFieldTypes.RichText;
 }
 
