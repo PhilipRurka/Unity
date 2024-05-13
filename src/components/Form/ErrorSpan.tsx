39910259030e2e6425e10d7afd2ef289236d3486
Philip Rurka <hey@philiprurka.com>
2024-05-13 14:44:12 -0400

    remove empty console .logs

--- src/components/Form/ErrorSpan.tsx
+++ src/components/Form/ErrorSpan.tsx
@@ -4,14 +4,10 @@ type ErrorSpanProps = HTMLProps<HTMLSpanElement> & {
   children: ReactNode;
 };
 
-const ErrorSpan = ({ children, ...rest }: ErrorSpanProps) => {
-  console.log('');
-
-  return (
-    <span className="" {...rest}>
-      {children}
-    </span>
-  );
-};
+const ErrorSpan = ({ children, ...rest }: ErrorSpanProps) => (
+  <span className="" {...rest}>
+    {children}
+  </span>
+);
 
 export default ErrorSpan;
