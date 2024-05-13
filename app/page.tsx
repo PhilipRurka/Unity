2c94fe3759a63043062ece2b986ebbb4f10b13ff
Philip Rurka <hey@philiprurka.com>
2024-05-13 14:09:48 -0400

    revert testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-const HomePage = () => <h1>ello</h1>;
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+
+import Home from '@/Components/Home';
+import authOptions from '@/Lib/authOptions';
+
+const HomePage = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect('articles/unity-race');
+  }
+
+  return <Home />;
+};
 
 export default HomePage;
