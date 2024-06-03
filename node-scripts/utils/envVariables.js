d180a9a5d7609a8d9e667b562a4dd2896a0a60e3
Philip Rurka <hey@philiprurka.com>
2024-06-03 09:02:51 -0400

    Scripts: update envVariables

--- node-scripts/utils/envVariables.js
+++ node-scripts/utils/envVariables.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from "url";
 const envVariables = () => {
   const filename = fileURLToPath(import.meta.url);
   const dirname = path.dirname(filename);
-  const envPath = path.resolve(dirname, "../../.env");
+  const envPath = path.resolve(dirname, "../../.env.local");
 
   dotenv.config({ path: envPath });
 
