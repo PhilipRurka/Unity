a464728d583622413cd4bdced6fb25f4ee6f2f83
Philip Rurka <hey@philiprurka.com>
2024-08-07 19:22:53 -0400

    Admin: Temporary Comment For Deployment

--- apps/unity-admin/app/users/page.tsx
+++ apps/unity-admin/app/users/page.tsx
@@ -1,15 +1,16 @@
-import UserTable from '@/Components/UserTable';
-import UsersFilter from '@/Components/UsersFilter';
-import getUsers from '@/Fetchers/getUsers';
+// import UserTable from '@/Components/UserTable';
+// import UsersFilter from '@/Components/UsersFilter';
+
+// import getUsers from '@/Fetchers/getUsers';
 
 const UsersPage = async () => {
-  const userList = await getUsers();
-
+  // const userList = await getUsers();
+  console.log('');
   return (
     <div data-component="UsersPage">
       <h1 className="my-9 text-4xl">List of Users</h1>
-      <UsersFilter usersLength={userList.length} />
-      <UserTable usersList={userList} />
+      {/* <UsersFilter usersLength={userList.length} /> */}
+      {/* <UserTable usersList={userList} /> */}
     </div>
   );
 };
