0cae2c3191f9192abd015562bbfc2f72fce66abc
Philip Rurka <hey@philiprurka.com>
2024-08-08 13:22:33 -0400

    Compendium, Admin: replace mongoConnect with connectToDatabase

---
+++ apps/unity-compendium/lib/connectToDatabase.ts
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+const connectToDatabase = async () => {
+  if (mongoose.connection.readyState === 1) {
+    return;
+  }
+
+  if (mongoose.connection.readyState === 2) {
+    await new Promise((resolve) => {
+      mongoose.connection.once('connected', resolve);
+    });
+
+    return;
+  }
+
+  mongoose.connect(`${process.env.MONGODB_URI}/${process.env.MONGODB_DATABASE}`);
+};
+
+export default connectToDatabase;
