fb9a63d94933636c1fe394156d33a3adbf11acf7
Philip Rurka <hey@philiprurka.com>
2024-08-25 11:05:33 -0400

    Components: adjust Input to include isInlineWithContent

--- sdk/components/src/Form/Input.tsx
+++ sdk/components/src/Form/Input.tsx
@@ -3,23 +3,27 @@ import React, { HTMLProps, forwardRef } from 'react';
 
 type InputProps = HTMLProps<HTMLInputElement> & {
   showErrorStyles?: boolean;
+  isInlineWithContent?: boolean;
   className?: string;
 };
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ showErrorStyles, className, ...rest }, ref) => (
-  <input
-    className={clsx(
-      'block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900',
-      'focus:border-blue-500 focus:ring-blue-500',
-      'dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500',
-      'read-only:border-transparent read-only:bg-transparent read-only:outline-none read-only:ring-transparent read-only:focus:border-transparent',
-      showErrorStyles ? 'border-red-500' : 'border-black',
-      className
-    )}
-    {...rest}
-    ref={ref}
-  />
-));
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ showErrorStyles, isInlineWithContent, className, ...rest }, ref) => (
+    <input
+      className={clsx(
+        'block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900',
+        'focus:border-blue-500 focus:ring-blue-500',
+        'dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500',
+        'read-only:border-transparent read-only:bg-transparent read-only:outline-none read-only:ring-transparent read-only:focus:border-transparent',
+        isInlineWithContent && '-ml-2.5',
+        showErrorStyles ? 'border-red-500' : 'border-black',
+        className
+      )}
+      {...rest}
+      ref={ref}
+    />
+  )
+);
 
 Input.displayName = 'Input';
 
