391503454ea925a7cbcb5104dfa300cbd8b8b3cf
Philip Rurka <hey@philiprurka.com>
2024-09-18 20:01:32 -0400

    Models: add user log option

--- packages/models/src/userLogs.ts
+++ packages/models/src/userLogs.ts
@@ -1,13 +1,21 @@
 import mongoose, { Schema, models } from 'mongoose';
 
-import type { ActiveSessionType, InviteSentType, StatusChangeType, UserLogsDocument } from '@unity/types';
+import type {
+  ActiveSessionType,
+  InviteSentType,
+  StatusChangeType,
+  UserLogsDocument,
+  UserUpdatedType,
+} from '@unity/types';
 
 const LogBaseSchema = new Schema(
   {
-    type: { type: String, required: true, enum: ['activeSession', 'statusChange', 'inviteSent'] },
+    type: { type: String, required: true, enum: ['activeSession', 'statusChange', 'inviteSent', 'userUpdated'] },
   },
   { discriminatorKey: 'type', _id: false }
 );
+
+/* #region Logs Schema Options */
 
 const ActiveSessionSchema = new Schema<ActiveSessionType>({
   timestamp: { type: Date, required: true },
@@ -24,7 +32,17 @@ const StatusChangeSchema = new Schema<StatusChangeType>({
   timestamp: { type: Date, required: true },
 });
 
+const UserUpdated = new Schema<UserUpdatedType>({
+  updatedProperties: [{ type: String }],
+  timestamp: { type: Date, required: true },
+});
+
+/* #endregion */
+
+/* #region Logs Schema Set */
+
 const LogModel = mongoose.models.Log || mongoose.model('Log', LogBaseSchema);
+
 if (!LogModel.discriminators || !LogModel.discriminators.activeSession) {
   LogModel.discriminator('activeSession', ActiveSessionSchema);
 }
@@ -37,17 +55,20 @@ if (!LogModel.discriminators || !LogModel.discriminators.inviteSent) {
   LogModel.discriminator('inviteSent', InviteSentSchema);
 }
 
-const userLogsSchema = new Schema<UserLogsDocument>(
-  {
-    user_id: {
-      type: Schema.Types.ObjectId,
-      required: true,
-      ref: 'User',
-    },
-    logs: { type: [LogBaseSchema], required: true },
+if (!LogModel.discriminators || !LogModel.discriminators.userUpdated) {
+  LogModel.discriminator('userUpdated', UserUpdated);
+}
+
+/* #endregion */
+
+const userLogsSchema = new Schema<UserLogsDocument>({
+  user_id: {
+    type: Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
   },
-  { timestamps: true }
-);
+  logs: { type: [LogBaseSchema], required: true },
+});
 
 const UserLogsModel = models.user_logs || mongoose.model<UserLogsDocument>('user_logs', userLogsSchema);
 
