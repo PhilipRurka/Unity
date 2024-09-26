72a3781e1bfc3f4f5dfbde09a752b24caae45c38
Philip Rurka <hey@philiprurka.com>
2024-09-26 12:35:42 -0400

    Model: Fix wrong import path

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -1,4 +1,4 @@
 export { default as ActivityAnalyticsModel } from './activityAnalytics';
 export { default as UserModel } from './user';
 export { default as UserLogsModel } from './userLogs';
-export { default as InternalToolsModel } from './InternalTools';
+export { default as InternalToolsModel } from './internalTools';
