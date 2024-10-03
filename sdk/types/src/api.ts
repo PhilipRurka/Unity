cef3d27e4a998d766829ff54efa680dd26b505e6
Philip Rurka <hey@philiprurka.com>
2024-10-03 10:14:14 -0400

    Types: remove unused commented types; add ApiMethodResponse;

--- sdk/types/src/api.ts
+++ sdk/types/src/api.ts
@@ -1,4 +1,5 @@
 export type SuccessGetType<T> = [{ result: T }, { status: number }];
 export type ErrorGetType = [{ error: { message: string } }, { status: number }];
 
-export type ApiMethodResponseType<T> = Promise<SuccessGetType<T> | ErrorGetType>;
+export type ApiMethodResponse<T> = SuccessGetType<T> | ErrorGetType;
+export type ApiMethodResponsePromise<T> = Promise<ApiMethodResponse<T>>;
--- sdk/types/src/internalTools.ts
+++ sdk/types/src/internalTools.ts
@@ -1,20 +1,21 @@
-import type { Document as ContentfulDocument } from '@contentful/rich-text-types';
 import mongoose from 'mongoose';
+
+export type AuditOptions = 'all' | 'algolia' | 'incomplete' | 'link placement';
+
+export type AuditOption = {
+  option: AuditOptions;
+};
 
 export type AuditType = {
   last_algolia_update?: Date;
   last_incomplete_update?: Date;
   last_link_placement_update?: Date;
-  incomplete_audit?: ContentfulDocument;
-  link_placement_audit?: ContentfulDocument;
 };
 
 export type AuditFrontendType = {
   lastAlgoliaUpdate?: Date;
   lastIncompleteUpdate?: Date;
   lastLinkPlacementUpdate?: Date;
-  incompleteAudit?: ContentfulDocument;
-  linkPlacementAudit?: ContentfulDocument;
 };
 
 export type InternalToolsDocument = mongoose.Document & AuditType;
