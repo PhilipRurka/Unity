8f9face3fdcf238de98e01305699c6ad63706165
Philip Rurka <hey@philiprurka.com>
2024-10-03 10:06:42 -0400

    Admin: add internalTools api

--- apps/unity-admin/app/api/internalTools/route.ts
+++ apps/unity-admin/app/api/internalTools/route.ts
@@ -1,16 +1,59 @@
 /* eslint-disable import/prefer-default-export */
 import { NextRequest, NextResponse } from 'next/server';
 
-import { checkIfAdminAuthenticated, updateInternalTools } from '@unity/api-methods';
-import { AuditFrontendType } from '@unity/types';
+import {
+  // checkIfAdminAuthenticated,
+  updateAlgolia,
+  updateIncomplete,
+  updateInternalTools,
+  updateLinkPlacement,
+} from '@unity/api-methods';
+import { AuditOption, AuditType } from '@unity/types';
+
+import contentfulEnvironment from '@/Lib/contentfulEnvironment';
 
 export const PUT = async (req: NextRequest) => {
-  const isAdminAuthenticated = await checkIfAdminAuthenticated(req);
-  if (!isAdminAuthenticated) return NextResponse.json({}, {});
+  // const isAdminAuthenticated = await checkIfAdminAuthenticated(req);
+  // if (!isAdminAuthenticated) return NextResponse.json({}, {});
 
-  const reqData: AuditFrontendType = await req.json();
+  const { option }: AuditOption = await req.json();
 
-  const [data, status] = await updateInternalTools(reqData);
+  const environment = await contentfulEnvironment();
+
+  let toolsToUpdate: AuditType = {};
+
+  switch (option) {
+    case 'algolia':
+      toolsToUpdate = await updateAlgolia();
+      break;
+
+    case 'incomplete':
+      toolsToUpdate = await updateIncomplete(environment);
+      break;
+
+    case 'link placement':
+      toolsToUpdate = await updateLinkPlacement(environment);
+      break;
+
+    case 'all': {
+      const algoliaObj = await updateAlgolia();
+      const incompleteObj = await updateIncomplete(environment);
+      const linkPlacementObj = await updateLinkPlacement(environment);
+
+      toolsToUpdate = {
+        ...algoliaObj,
+        ...incompleteObj,
+        ...linkPlacementObj,
+      };
+
+      break;
+    }
+
+    default:
+      break;
+  }
+
+  const [data, status] = await updateInternalTools(toolsToUpdate);
 
   return NextResponse.json(data, status);
 };
