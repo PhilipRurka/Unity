1dbcf1b3f9f63dc9e1a5163b5390714563218e80
Philip Rurka <hey@philiprurka.com>
2024-10-03 10:13:01 -0400

    Models: remove unused comments;

--- packages/models/src/internalTools.ts
+++ packages/models/src/internalTools.ts
@@ -7,8 +7,6 @@ const InternalToolsSchema = new Schema<InternalToolsDocument>(
     last_algolia_update: { type: Date },
     last_incomplete_update: { type: Date },
     last_link_placement_update: { type: Date },
-    incomplete_audit: { type: Schema.Types.Mixed },
-    link_placement_audit: { type: Schema.Types.Mixed },
   },
   { timestamps: true }
 );
