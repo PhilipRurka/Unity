e8ae2d564f3e4bbeff0c203483ff153a1f60db49
Philip Rurka <hey@philiprurka.com>
2024-12-10 14:39:50 -0500

    Admin: update getContentModule Types

--- apps/unity-admin/src/fetchers/getContentModel.ts
+++ apps/unity-admin/src/fetchers/getContentModel.ts
@@ -1,6 +1,6 @@
-import type { FetchErrorType, GetContentModelType } from '@unity/types';
+import type { FetchErrorType, GetByContentModel } from '@unity/types';
 
-const getContentModel: GetContentModelType = async (type) => {
+const getContentModel: GetByContentModel = async (type) => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/contentful/contentModel/${type}`, {
     method: 'GET',
     headers: {
--- apps/unity-admin/src/hooks/useContentModel.ts
+++ apps/unity-admin/src/hooks/useContentModel.ts
@@ -1,12 +1,10 @@
-import useSWR from 'swr';
+import useSWR, { SWRResponse } from 'swr';
 
 import { AllContentModelTypes, ContentModelMapping } from '@unity/types';
 
 import getContentModel from '@/Fetchers/getContentModel';
 
-export type UseContentModel = <T extends AllContentModelTypes>(
-  type: T
-) => ReturnType<typeof useSWR<Array<ContentModelMapping[T]>>>;
+export type UseContentModel = <T extends AllContentModelTypes>(type: T) => SWRResponse<ContentModelMapping[T][], any>;
 
 const useContentModel: UseContentModel = <T extends AllContentModelTypes>(type: T) => {
   const response = useSWR<Array<ContentModelMapping[T]>>(`content-model-${type}`, () => getContentModel<T>(type));
