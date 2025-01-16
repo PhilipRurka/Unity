b429cdbc840899615ad455c7555e6829ae478833
Philip Rurka <hey@philiprurka.com>
2025-01-16 11:01:47 -0500

    Admin: re-factor & query all for ContentfulLayoutAdjust

--- apps/unity-admin/src/components/ContentfulLayoutAdjust/ContentfulLayoutAdjust.tsx
+++ apps/unity-admin/src/components/ContentfulLayoutAdjust/ContentfulLayoutAdjust.tsx
@@ -1,34 +1,49 @@
 import { Button } from '@unity/components';
 
 const ContentfulLayoutAdjust = () => {
+  const script = `
+    window.unity = {
+      isHideSidebar : false
+    }
+
+    window.unity.hideSidebar = () => {
+      document.querySelectorAll('[data-test-id="cf-layout-sidebar"]').forEach((element) => {
+        element.style.display = 'none';
+      })
+
+      document.querySelectorAll('[data-test-id="cf-layout-content-container"]').forEach((element) => {
+        element.style.display = 'block';
+      })
+    }
+
+    window.unity.showSidebar = () => {
+      document.querySelectorAll('[data-test-id="cf-layout-sidebar"]').forEach((element) => {
+        element.style.display = 'block';
+      })
+
+      document.querySelectorAll('[data-test-id="cf-layout-content-container"]').forEach((element) => {
+        element.style.display = 'grid';
+      })
+    }
+
+    document.addEventListener('keydown', (event) => {
+      if (event.key === '\\\\') {
+        event.preventDefault()
+        
+        if (window.unity.isHideSidebar) {
+          window.unity.showSidebar()
+        } else {
+          window.unity.hideSidebar()
+        }
+
+        window.unity.isHideSidebar = !window.unity.isHideSidebar
+      }
+    });
+  `;
+
   const handleCopyContentfulLayoutToClipboard = () => {
     try {
-      navigator.clipboard.writeText(`
-        window.unity = {
-          isHideSidebar : false
-        }
-        
-        window.unity.hideSidebar = () => {
-          document.querySelector('[data-test-id="cf-layout-sidebar"]').style.display = 'none';
-          document.querySelector('[data-test-id="cf-layout-content-container"]').style.display = 'block';
-        }
-        
-        window.unity.showSidebar = () => {
-          document.querySelector('[data-test-id="cf-layout-sidebar"]').style.display = 'block';
-          document.querySelector('[data-test-id="cf-layout-content-container"]').style.display = 'grid';
-        }
-        
-        document.addEventListener('keydown', (event) => {
-          if (event.key === '\\\\') {
-            if (window.unity.isHideSidebar) {
-              window.unity.showSidebar()
-            } else {
-              window.unity.hideSidebar()
-            }
-            window.unity.isHideSidebar = !window.unity.isHideSidebar
-          }
-        });
-      `);
+      navigator.clipboard.writeText(script);
     } catch (err) {
       // eslint-disable-next-line no-console
       console.error('Failed to copy text: ', err);
@@ -40,35 +55,7 @@ const ContentfulLayoutAdjust = () => {
       <Button color="black" size="small" onClick={handleCopyContentfulLayoutToClipboard}>
         Copy Code
       </Button>
-      <pre className="mt-6 text-xs">
-        {`
-          window.unity = {
-            isHideSidebar : false
-          }
-
-          window.unity.hideSidebar = () => {
-            document.querySelector('[data-test-id="cf-layout-sidebar"]').style.display = 'none';
-            document.querySelector('[data-test-id="cf-layout-content-container"]').style.display = 'block';
-          }
-
-          window.unity.showSidebar = () => {
-            document.querySelector('[data-test-id="cf-layout-sidebar"]').style.display = 'block';
-            document.querySelector('[data-test-id="cf-layout-content-container"]').style.display = 'grid';
-          }
-
-          document.addEventListener('keydown', (event) => {
-            if (event.key === '\\\\') {
-              if (window.unity.isHideSidebar) {
-                window.unity.showSidebar()
-              } else {
-                window.unity.hideSidebar()
-              }
-
-              window.unity.isHideSidebar = !window.unity.isHideSidebar
-            }
-          });
-        `}
-      </pre>
+      <pre className="mt-6 text-xs">{script}</pre>
     </>
   );
 };
