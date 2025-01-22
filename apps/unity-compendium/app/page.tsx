2f80db952951f882b61c1d9b89c0e9aecfa7df84
Philip Rurka <hey@philiprurka.com>
2025-01-22 13:38:59 -0500

    Compendium: Build out the homepage

--- apps/unity-compendium/app/page.tsx
+++ apps/unity-compendium/app/page.tsx
@@ -1,5 +1,12 @@
-import Home from '@/Components/Home';
+import { HomepageType } from '@unity/types';
 
-const HomePage = async () => <Home />;
+import Home from '@/Components/Home';
+import getByContentModel from '@/Fetchers/contentful/getByContentModel';
+
+const HomePage = async () => {
+  const data: HomepageType[] = await getByContentModel('homepage');
+
+  return <Home data={data[0]} />;
+};
 
 export default HomePage;
--- apps/unity-compendium/src/components/Home/Home.tsx
+++ apps/unity-compendium/src/components/Home/Home.tsx
@@ -1,5 +1,44 @@
 'use client';
 
-const Home = () => <h2>Welcome Home</h2>;
+import clsx from 'clsx';
+import { Fragment } from 'react';
+
+import { Markdown } from '@unity/components';
+import { HomepageType } from '@unity/types';
+
+type HomeProps = {
+  data: HomepageType;
+};
+
+const Home = ({
+  data: {
+    fields: { title, content },
+  },
+}: HomeProps) => (
+  <main
+    className={clsx(
+      'mx-auto min-h-screen max-w-4xl bg-white bg-opacity-90 px-8 pb-20 pt-32 shadow-article transition-opacity',
+      'sm:mt-16 sm:px-16 sm:py-20'
+    )}
+    data-component="cArticle"
+  >
+    <h1 className="mb-8 text-4xl md:text-5xl lg:text-6xl">{title}</h1>
+    {content.map((section, i) => {
+      if (!section?.fields) return <Fragment key={`Article-${i}`} />;
+
+      return (
+        <div key={`Article-${i}`} className="mb-16">
+          {section?.fields.title && (
+            <>
+              <h2 className={clsx('relative text-3xl lg:text-4xl')}>{section?.fields.title}</h2>
+              <hr className=" mb-4 border-black" />
+            </>
+          )}
+          <Markdown content={section?.fields.content} />
+        </div>
+      );
+    })}
+  </main>
+);
 
 export default Home;
