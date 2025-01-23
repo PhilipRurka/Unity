0859ecbf431d116aac51ed84bab4d9272a16c7d0
Philip Rurka <hey@philiprurka.com>
2025-01-23 14:11:11 -0500

    Compendium: implement metadata

--- apps/unity-compendium/app/layout.tsx
+++ apps/unity-compendium/app/layout.tsx
@@ -1,9 +1,10 @@
 import clsx from 'clsx';
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import { Lexend, Montserrat } from 'next/font/google';
 
 import '@unity/styles/global.css';
 
+import AddedHead from '@/Components/AddedHead';
 import HeaderServer from '@/Components/Header/HeaderServer';
 import HierarchyNav from '@/Components/HierarchyNav';
 import SearchModal from '@/Components/SearchModal';
@@ -29,7 +30,64 @@ const montserrat = Montserrat({
 
 export const metadata: Metadata = {
   title: 'Unity Compendium',
-  robots: 'noindex, nofollow',
+  description:
+    'Immerse yourself in a meticulously crafted fantasy world filled with rich lore, breathtaking landscapes, and unique creations. Explore a universe brought to life through detailed world-building, where imagination and creativity shape every corner.',
+  keywords: [
+    'fantasy',
+    'world-building',
+    'Unity Compendium',
+    'lore',
+    'legend',
+    'legends',
+    'myth',
+    'myths',
+    'culture',
+    'unity',
+    'ceremony',
+    'ceremonies',
+    'conflict',
+    'conflicts',
+    'deity',
+    'deities',
+    'god',
+    'gods',
+    'art',
+    'imagination',
+    'creation',
+  ],
+  authors: { name: 'Philip Rurka' },
+  creator: 'Philip Rurka',
+  publisher: 'Philip Rurka',
+  openGraph: {
+    images: '/unity-logo.png',
+    locale: 'en_US',
+    type: 'article',
+    url: 'https://unitystory.info',
+    siteName: 'Unity Compendium',
+    authors: ['Philip Rurka'],
+  },
+  icons: {
+    icon: '/unity-logo.png',
+  },
+  verification: {
+    google: 'google',
+  },
+  assets: [
+    'https://fonts.googleapis.com/css2?family=Lexend:wght@300;500&display=swap',
+    'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500&display=swap',
+    '/layout-background.jpg',
+    '/search-modal-background.jpg',
+    '/unity-logo.png',
+  ],
+  category: 'Fantasy',
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  themeColor: '#000000',
 };
 
 type RootLayoutProps = {
@@ -39,6 +97,7 @@ type RootLayoutProps = {
 export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en">
+      <AddedHead />
       <body
         className={clsx(
           'hide-scrollbar relative h-full min-h-screen w-screen overflow-x-hidden bg-layout bg-cover bg-fixed bg-bottom bg-no-repeat text-ocean-blue',
