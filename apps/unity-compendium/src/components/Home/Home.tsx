387ee06c1824ee0fdf5240530f6d7feef68b1e38
Philip Rurka <hey@philiprurka.com>
2025-01-22 21:14:35 -0500

    Compendium: now this

--- apps/unity-compendium/src/components/Home/Home.tsx
+++ apps/unity-compendium/src/components/Home/Home.tsx
@@ -4,29 +4,27 @@ import clsx from 'clsx';
 
 // import { Fragment } from 'react';
 // import { Markdown } from '@unity/components';
-import { HomepageType } from '@unity/types';
+// import { HomepageType } from '@unity/types';
 
-type HomeProps = {
-  data: HomepageType;
-};
+// type HomeProps = {
+//   data: HomepageType;
+// };
 
-const Home = ({
-  data: {
-    fields: { title, content },
-  },
-}: HomeProps) => {
-  console.log(content);
-
-  return (
-    <main
-      className={clsx(
-        'mx-auto min-h-screen max-w-4xl bg-white bg-opacity-90 px-8 pb-20 pt-32 shadow-article transition-opacity',
-        'sm:mt-16 sm:px-16 sm:py-20'
-      )}
-      data-component="cArticle"
-    >
-      {/* <h1 className="mb-8 text-4xl md:text-5xl lg:text-6xl">{title}</h1> */}
-      {/* {content.map((section, i) => {
+const Home = () => (
+  // const Home = ({
+  //   data: {
+  //     fields: { title, content },
+  //   },
+  // }: HomeProps) => {
+  <main
+    className={clsx(
+      'mx-auto min-h-screen max-w-4xl bg-white bg-opacity-90 px-8 pb-20 pt-32 shadow-article transition-opacity',
+      'sm:mt-16 sm:px-16 sm:py-20'
+    )}
+    data-component="cArticle"
+  >
+    {/* <h1 className="mb-8 text-4xl md:text-5xl lg:text-6xl">{title}</h1> */}
+    {/* {content.map((section, i) => {
         if (!section?.fields) return <Fragment key={`Article-${i}`} />;
   
         return (
@@ -41,8 +39,7 @@ const Home = ({
           </div>
         );
       })} */}
-    </main>
-  );
-};
+  </main>
+);
 
 export default Home;
