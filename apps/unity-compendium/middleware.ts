1c6909efcb38783cceac229a2242e099508f9e31
Philip Rurka <hey@philiprurka.com>
2025-01-23 14:10:47 -0500

    Compendium: adjust middleware redirect from /login to /

--- apps/unity-compendium/middleware.ts
+++ apps/unity-compendium/middleware.ts
@@ -30,7 +30,7 @@ export default async function middleware(req: NextRequest) {
     }
 
     if (!isAuthPath && pathname !== '/') {
-      return NextResponse.redirect(new URL('/login', req.url));
+      return NextResponse.redirect(new URL('/', req.url));
     }
 
     return NextResponse.next();
