579e8e89c8b0a651307faabe7088cd2e1fa87531
Philip Rurka <hey@philiprurka.com>
2025-01-30 12:09:32 -0500

    Admin: tweak Users

--- apps/unity-admin/src/components/Users/Users.tsx
+++ apps/unity-admin/src/components/Users/Users.tsx
@@ -1,19 +1,11 @@
 'use client';
 
-import UsersFilter from '@/Components/UsersFilter';
-import useUsers from '@/Hooks/useUsers';
-
 import UsersTabs from '../UsersTabs';
 
-const Users = () => {
-  const { data: usersList = [] } = useUsers();
-
-  return (
-    <div data-component="Users">
-      <UsersFilter usersLength={usersList.length} />
-      <UsersTabs />
-    </div>
-  );
-};
+const Users = () => (
+  <div data-component="Users">
+    <UsersTabs />
+  </div>
+);
 
 export default Users;
--- apps/unity-admin/src/components/UsersTabs/UsersTabs.tsx
+++ apps/unity-admin/src/components/UsersTabs/UsersTabs.tsx
@@ -1,9 +1,11 @@
 import Tabs from '@/Components/Tabs';
+import useUsers from '@/Hooks/useUsers';
 
+import UsersFilter from '../UsersFilter';
 import UsersTable from '../UsersTable';
 
 const UserTableTabs = () => {
-  console.log();
+  const { data: usersList = [] } = useUsers();
 
   return (
     <div data-component="UserTableTabs" className="mt-16">
@@ -15,6 +17,7 @@ const UserTableTabs = () => {
         <Tabs.Content>
           <Tabs.View view="active">
             <h1 className="my-9 text-4xl">List of Users</h1>
+            <UsersFilter usersLength={usersList.length} />
             <UsersTable />
           </Tabs.View>
           <Tabs.View view="registration-request">
