309df7739ffe278a527feb733168eeb941f2c7c7
Philip Rurka <hey@philiprurka.com>
2025-04-04 10:29:42 -0400

    Types: add registrationRequest types

--- sdk/types/src/registrationRequest.ts
+++ sdk/types/src/registrationRequest.ts
@@ -1,15 +1,24 @@
 import mongoose from 'mongoose';
 
-export type RegistrationRequestType = {
+export type RegistrationRequestReq = {
   name: string;
   email: string;
   message?: string;
 };
 
+export type RegistrationRequestType = {
+  name: string;
+  email: string;
+  message?: string;
+  created_at: Date;
+};
+
 export type RegistrationRequestFrontendType = {
+  id: string;
   name: string;
   email: string;
   message: string;
+  createdAt: Date;
 };
 
 export type RegistrationRequestDocument = mongoose.Document & RegistrationRequestType;
