2429e2fd2fb367f5142d26ec453066a49bd6bfa9
Philip Rurka <hey@philiprurka.com>
2025-08-28 14:16:24 -0400

    Compendium: adjust Rich Text for google search results

--- apps/unity-compendium/src/components/AddedHead/AddedHead.tsx
+++ apps/unity-compendium/src/components/AddedHead/AddedHead.tsx
@@ -7,23 +7,47 @@ const AddedHead = () => (
       dangerouslySetInnerHTML={{
         __html: JSON.stringify({
           '@context': 'https://schema.org',
-          '@type': 'CreativeWork',
-          name: 'Unity Compendium',
-          description:
-            'Immerse yourself in a meticulously crafted fantasy world filled with rich lore, breathtaking landscapes, and unique creations.',
-          author: { '@type': 'Person', name: 'Philip Rurka' },
-          creator: { '@type': 'Person', name: 'Philip Rurka' },
-          publisher: { '@type': 'Person', name: 'Philip Rurka' },
-          keywords: 'fantasy, world-building, lore, Unity Compendium',
-          genre: 'Fantasy',
-          inLanguage: 'en-US',
-          url: 'https://www.unitystory.info',
-          image: 'https://www.unitystory.info/unity-logo.png',
-          mainEntityOfPage: {
-            '@type': 'WebPage',
-            '@id': 'https://www.unitystory.info',
-          },
-        }),
+          '@graph': [
+            {
+              '@type': 'CreativeWorkSeries',
+              name: 'Unity Compendium',
+              description:
+                'Immerse yourself in a meticulously crafted fantasy world filled with rich lore, breathtaking landscapes, and unique creations.',
+              author: {
+                '@type': 'Person',
+                name: 'Philip Rurka'
+              },
+              creator: {
+                '@type': 'Person',
+                name: 'Philip Rurka'
+              },
+              publisher: {
+                '@type': 'Person',
+                name: 'Philip Rurka'
+              },
+              genre: 'Fantasy',
+              keywords: ['fantasy', 'world-building', 'lore', 'Unity Compendium'],
+              inLanguage: 'en-US',
+              url: 'https://www.unitystory.info',
+              image: 'https://www.unitystory.info/unity-logo.png',
+              mainEntityOfPage: {
+                '@type': 'WebPage',
+                '@id': 'https://www.unitystory.info'
+              }
+            },
+            {
+              '@type': 'WebSite',
+              url: 'https://www.unitystory.info',
+              name: 'Unity Compendium'
+            },
+            {
+              '@type': 'Organization',
+              name: 'Unity Compendium',
+              url: 'https://www.unitystory.info',
+              logo: 'https://www.unitystory.info/unity-logo.png'
+            }
+          ]
+        })
       }}
     />
   </Head>
