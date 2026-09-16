5e45aef5620da45f826b0fc60848f06334d836b2
Philip Rurka <hey@philiprurka.com>
2026-09-16 19:13:53 -0400

    Test: test complete, revert test

--- apps/unity-compendium/app/page.tsx
+++ apps/unity-compendium/app/page.tsx
@@ -8,11 +8,6 @@ type Result = {
 }
 
 const HomePage = async () => {
-  console.log('Contentful env from page:', {
-    hasSpaceId: Boolean(process.env.CONTENTFUL_SPACE_ID),
-    hasAccessToken: Boolean(process.env.CONTENTFUL_ACCESS_TOKEN),
-  });
-
   const data = await getByContentModel('homepage');
 
   if (!Array.isArray(data) || !data[0]) return <></>;
