1487629dde0045ce41448f8e3d0a7a67cbed285e
Philip Rurka <hey@philiprurka.com>
2026-09-17 13:22:52 -0400

    Update: resolve some easy vscode ts issues and build warnings

--- packages/next-config/index.js
+++ packages/next-config/index.js
@@ -1,12 +1,13 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
+
 const nextConfig = {
   poweredByHeader: false,
   onDemandEntries: {
     maxInactiveAge: 25 * 1000,
     pagesBufferLength: 2,
-  },
-  eslint: {
-    // ignoreDuringBuilds: true,
   },
 };
 
